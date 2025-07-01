e6ba0a01b63b50dd3df3a659108061f6872d37df
niki1tusa <niki2ta00n4@gmail.com>
2025-07-01 19:01:46 +0300

    replace section to buttons block

--- src/components/dashboard/last-tasks/LastTasks.tsx
+++ src/components/dashboard/last-tasks/LastTasks.tsx
@@ -17,30 +17,34 @@ export const LastTasks = () => {
 	const count = filteredTasks.length;
 	return (
 		<div className='mt-5'>
-			<h1 className='mb-3 text-[22px] font-medium'>
-				Last Tasks <span className='opacity-50'>({count})</span>
-			</h1>
-			<div>
-				<div className='text-right text-sm'>
-					<select
-						onChange={e => setSelect(e.target.value)}
-						className='mb-4 rounded-[4px] border border-white bg-white py-1 pl-2 shadow shadow-neutral-400'
-					>
-						{OPTIONS.map(option => (
-							<option key={option} value={option}>
-								{option}
-							</option>
-						))}
-					</select>
+			<div className='mt-10 flex justify-between'>
+				<h1 className='mb-3 text-[22px] font-medium'>
+					Last Tasks <span className='opacity-50'>({count})</span>
+				</h1>
+				<div className='mb-4 flex gap-2 rounded-[4px] border border-white py-1 pl-2 shadow shadow-neutral-400'>
+					{OPTIONS.map(button => (
+						<button
+							onClick={() => setSelect(button)}
+							key={button}
+							value={button}
+							className={`rounded-sm px-4 py-1.5 text-sm font-medium transition-all duration-300 ${
+								select === button
+									? 'text-primary bg-white shadow'
+									: 'hover:text-primary text-gray-500'
+							}`}
+						>
+							{button}
+						</button>
+					))}
 				</div>
+			</div>
 
-				<div className='grid grid-cols-3 gap-2'>
-					{count ? (
-						filteredTasks.map(task => <Task key={task.id} task={task} />)
-					) : (
-						<div>No tasks found.</div>
-					)}
-				</div>
+			<div className='grid grid-cols-3 gap-2'>
+				{count ? (
+					filteredTasks.map(task => <Task key={task.id} task={task} />)
+				) : (
+					<div>No tasks found.</div>
+				)}
 			</div>
 		</div>
 	);
