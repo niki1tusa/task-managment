ffe81f0004d35a6b6b362cce300c56b1f56d5d3d
nikita <niki2ta00n4@gmail.com>
2025-07-04 08:41:43 +0300

    Fix modals (delete public directory)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,13 +5,13 @@ interface Props {
 	children: React.ReactNode;
 	modals: React.ReactNode;
 }
-export default function Layout({ children, modals }: Props) {
+export default function DashboardLayout({ children, modals }: Props) {
 	console.log('modals:', modals)
 	return (
 		<div className='grid grid-cols-[15%_60%_25%] justify-between '>
 			<Sidebar />
 			<main className='flex-1 dark:border-r dark:border-l dark:border-neutral-800'>
-		{modals && <div className='z-50 bg-amber-600 w-[300px] h-[500px] border border-amber-950' >{modals}</div>} {children}
+		{modals} {children}
 			</main>
 			<Chat/>
 		</div>
